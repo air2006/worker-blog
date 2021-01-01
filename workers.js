6052e1732f70b26bd2885f576646aee1901e9fd0
air2006 <zbj04142006@outlook.com>
2021-01-01 21:46:32 +0800

    初始化

--- workers.js
+++ workers.js
@@ -7,10 +7,10 @@
  */
 
 // 定义 Github 项目，文章会从这里读取
-const github_base = "kasuganosoras/cloudflare-worker-blog";
+const github_base = "air2006/cloudflare-worker-blog";
 
 // 站点名称
-const site_name = "CloudFlare Workers Blog";
+const site_name = "air2006的博客";
 
 // 站点介绍
 const site_desc = "一个用 CloudFlare Worker 搭建的博客";
@@ -21,7 +21,7 @@ const highlight_main = "https://cn.tql.ink:4443/js/highlight.min.js";
 const highlight_pack = "https://cn.tql.ink:4443/js/highlight.pack.js";
 
 // Copyright
-const copyright = `&copy; 2019 ${site_name} | <a href="https://github.com/kasuganosoras/cloudflare-worker-blog" target="_blank">Github</a>`;
+const copyright = `&copy; 2019 ${site_name} | <a href="https://github.com/air2006/cloudflare-worker-blog" target="_blank">Github</a>`;
 
 /* 配置到此结束 */
 
